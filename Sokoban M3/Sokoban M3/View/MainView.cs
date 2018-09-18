475225c7015b9e2d51f0d0ec2f2c1cee7d649550
Billyyy013 <bhfnooij@avans.nl>
2018-09-18 20:32:01 +0200

    printgame

--- Sokoban M3/Sokoban M3/View/MainView.cs
+++ Sokoban M3/Sokoban M3/View/MainView.cs
@@ -8,5 +8,23 @@ namespace Sokoban_M3.View
 {
     class MainView
     {
+
+        public void printGame()
+        {
+            Console.WriteLine("┌─────────────────────────────────────────────────────────┐");
+            Console.WriteLine("| Welkom bij Sokoban                                      |");
+            Console.WriteLine("|                                                         |");
+            Console.WriteLine("| Betekenis van de symbolen    |    Doel van het spel     |");
+            Console.WriteLine("|                              |                          |");
+            Console.WriteLine("| spatie : outerspace          |    Duw met de truck      |");
+            Console.WriteLine("|      █ : muur                |    De krat(ten)          |");
+            Console.WriteLine("|      · : vloer               |    naar de bestemming    |");
+            Console.WriteLine("|      O : krat                |                          |");
+            Console.WriteLine("|      0 : krat op bestemming  |                          |");
+            Console.WriteLine("|      x : bestemming          |                          |");
+            Console.WriteLine("|      @ : truck               |                          |");
+            Console.WriteLine("└─────────────────────────────────────────────────────────┘");
+            
+        }
     }
 }
