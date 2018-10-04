593a7b7a0a791ec285222378a772baf36f8d9bbd
Billyyy013 <bhfnooij@avans.nl>
2018-10-04 12:05:21 +0200

    awakemethod

--- Sokoban M3/Sokoban M3/Model/Worker.cs
+++ Sokoban M3/Sokoban M3/Model/Worker.cs
@@ -8,13 +8,35 @@ namespace Sokoban_M3.Model
 {
     class Worker : Entity
     {
+        private Random randomGen;
+        private bool Awake { get; set; }
         public Worker()
         {
             Symbol = '$';
             SymbolOnDestination = '$';
-        }
-        public override void ArrivedOnDestination(Maze maze, int value){return;}
 
-        public override void StepOnPitFall(Tile current){return;}
+            randomGen = new Random();
+        }
+        public override void ArrivedOnDestination(Maze maze, int value) { return; }
+
+        public override void StepOnPitFall(Tile current) { return; }
+
+        public bool CheckIfAwake()
+        {
+            int chance = randomGen.Next(1, 100);
+            if (Awake)
+            {
+                chance = randomGen.Next(1, 100);
+                if(chance > 0 && chance < 26)
+                {
+                    Awake = false;
+                }
+            }
+            if (chance > 0 && chance < 11)
+            {
+                Awake = true;
+            }
+            return Awake;
+        }
     }
 }
