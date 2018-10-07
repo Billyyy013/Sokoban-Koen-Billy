4732480383961c2cad0de25a87b164b0b4e4995c
Koen B <khcbaude@avans.nl>
2018-10-07 21:53:26 +0200

    Delete unnesecairy usings

--- Sokoban M3/Sokoban M3/Process/Parser.cs
+++ Sokoban M3/Sokoban M3/Process/Parser.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Sokoban_M3.Model;
 
 namespace Sokoban_M3.Process
--- Sokoban M3/Sokoban M3/Program.cs
+++ Sokoban M3/Sokoban M3/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sokoban_M3.Presentation;
-
-namespace Sokoban_M3
+﻿namespace Sokoban_M3
 {
     class Program
     {
